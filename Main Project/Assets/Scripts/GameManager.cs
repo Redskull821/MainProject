7c909fbe9ece84b21ed3fe28f3d947661bf1e69a
Joey <redskull821@gmail.com>
2023-10-17 17:39:56 -0400

    Milestone 1 In Progress

--- Main Project/Assets/Scripts/GameManager.cs
+++ Main Project/Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Tracker : MonoBehaviour
 {
+
+    //[SerializeField] TextMeshProUGUI roundTrackerText; 
+    //[SerializeField] TextMeshProUGUI turnTrackerText; 
+    private float roundTracker;
+    private float turnTracker;
     // Start is called before the first frame update
     void Start()
     {
