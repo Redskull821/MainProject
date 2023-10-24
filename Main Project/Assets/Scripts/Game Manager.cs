247f9eebb91dc0039cf04f371529d4910bdea6ec
Joey <redskull821@gmail.com>
2023-10-24 17:50:18 -0400

    Milestone 2 In Progress

--- Main Project/Assets/Scripts/Game Manager.cs
+++ Main Project/Assets/Scripts/Game Manager.cs
@@ -7,23 +7,45 @@ using UnityEngine.SceneManagement;
 public class Tracker : MonoBehaviour
 {
 
-    [SerializeField] TextMeshProUGUI roundTrackerText; 
-    private float roundTracker;
-    private float turnTracker;
+    [SerializeField] TextMeshProUGUI roundTrackerText;
     [SerializeField] float playerHealth = 10f;
     [SerializeField] float enemyHealth = 10f;
+    private float roundTracker = 0f;
+    private float turnTracker = 0f;
+    GameObject[] savedPlayerShips;
+    GameObject[] activeShips;
 
     // Start is called before the first frame update
     void Start()
     {
-        roundTracker = 1f;
-        turnTracker = 1f;
+        RoundIncrease();
+        TurnIncrease();
+        // grab all player ships and store them in savedPlayerShips
+        // grab all ships, and put them into activeShips
     }
 
     // Update is called once per frame
     void Update()
     {
+        // run through activeShips
+        //      perform Move() for each ship
+        // clear activeShips
+        // grab all ships, put them into activeShips
+        for (int i =0; i < activeShips.Length; i++)
+        {
+            PlayerShip ship = activeShips[i];
+            ship.Move();
+        }
+        TurnIncrease();
+        if (enemyHealth <= 0)
+        {
+            // initiate RoundEnd
+        }
+    }
 
+    private void RoundEnd()
+    {
+        ResetTurn();
     }
 
     public void StartGame()
--- Main Project/Assets/Scripts/PlayerShip.cs
+++ Main Project/Assets/Scripts/PlayerShip.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
+    [SerializeField] float health = 3f;
+    [SerializeField] float dmg = 2f;
+    private bool isEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,32 @@ public class PlayerShip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if collisiion detected, perform Hit()
+    }
+
+    public void Status(bool status)
+    {
+        isEnemy = status;
+    }
+
+    private void Hit(float dmg)
+    {
+        // destroy object on collision
+        // subtract damage from health total
+        // if health is <= 0, destroy the ship
+    }
+
+    private void Move()
+    {
+        // check if object is infront of ship
+        // if not
+        //      move forward one square
+        // else if object is enemy ship
+        //      Attack()
+    }
+
+    private void Attack()
+    {
+        // deal damage to object
     }
 }
