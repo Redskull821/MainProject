49e69c33adf4b7d288965a73bf18d006cf9584d4
Joey <redskull821@gmail.com>
2023-10-26 18:05:48 -0400

    Milestone 2 In Progress

--- Main Project/Assets/Scripts/Game Manager.cs
+++ Main Project/Assets/Scripts/Game Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,17 +15,23 @@ public class GameManager : MonoBehaviour
     [SerializeField] float enemyHealth = 10f;
     private float roundTracker = 1f;
     private float turnTracker = 1f;
-    //PlayerShip[] savedPlayerShips;
-    PlayerShip[] activeShips;
+    List<PlayerShip> savedPlayerShips;
+    List<PlayerShip> activeShips;
 
     // Start is called before the first frame update
     void Start()
     {
         // grab all ships, and put them into activeShips
 
-        // for all ships in activeShips
-        //      if ship isEnemy is false
-        //          add to savedPlayerShips
+        for (int i = 0; i < activeShips.Count; i++)
+        {
+            PlayerShip ship = activeShips[i];
+        
+            if (!ship.CheckStatus())
+            {
+                savedPlayerShips.Add(ship);
+            }
+        }
     }
 
     // Update is called once per frame
